c96a9b149e4636b3964d57fc68a9b5eca5ac826d
Renan Bezerra Guimaraes <renanbezerraguima@proton.me>
2023-12-22 22:34:12 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+line01 = "************************"
+line02 = "*                      *"
+line03 = "*       WELCOME!       *"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+a =  int(input("Insira um numero: "))
+while (a != -1):
+    print(a*3)
+    a =  int(input("Insira um numero: "))
